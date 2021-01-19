c0e681c4f74dcd8bd765c38068d35b1a5fd62aa3
lady ada <limor@ladyada.net>
2021-01-18 21:27:23 -0500

    add basic config port and write/read

--- Adafruit_AW9523.cpp
+++ Adafruit_AW9523.cpp
@@ -74,3 +74,37 @@ bool Adafruit_AW9523::reset(void) {
   Adafruit_I2CRegister resetreg = Adafruit_I2CRegister(i2c_dev, AW9523_REG_SOFTRESET);
   return resetreg.write(0);
 }
+
+
+bool Adafruit_AW9523::outputGPIO(uint16_t pins) {
+  Adafruit_I2CRegister output0reg = Adafruit_I2CRegister(i2c_dev, AW9523_REG_OUTPUT0, 2, LSBFIRST);
+  if (! output0reg.write(pins)) {
+    return false;
+  }
+  _pin_outputs = pins;
+
+  return true;
+}
+
+uint16_t Adafruit_AW9523::inputGPIO(void) {
+  Adafruit_I2CRegister input0reg = Adafruit_I2CRegister(i2c_dev, AW9523_REG_INPUT0, 2, LSBFIRST);
+  return input0reg.read();
+}
+
+bool Adafruit_AW9523::configureGPIO(uint16_t pins) {
+  Adafruit_I2CRegister conf0reg = Adafruit_I2CRegister(i2c_dev, AW9523_REG_CONFIG0, 2, LSBFIRST);
+  if (! conf0reg.write(~pins)) {
+    return false;
+  }
+
+  return true;
+}
+
+bool Adafruit_AW9523::openDrainPort0(bool od) {
+  Adafruit_I2CRegister gcrreg = Adafruit_I2CRegister(i2c_dev, AW9523_REG_GCR, 1);
+
+  Adafruit_I2CRegisterBits opendrain =
+      Adafruit_I2CRegisterBits(&gcrreg, 1, 4); // # bits, bit_shift
+
+  return opendrain.write(!od);
+}
--- Adafruit_AW9523.h
+++ Adafruit_AW9523.h
@@ -24,6 +24,10 @@
 #define AW9523_DEFAULT_ADDR 0x58
 #define AW9523_REG_CHIPID 0x10
 #define AW9523_REG_SOFTRESET 0x7F
+#define AW9523_REG_INPUT0 0x00
+#define AW9523_REG_OUTPUT0 0x02
+#define AW9523_REG_CONFIG0 0x04
+#define AW9523_REG_GCR 0x11
 
 /*!
  *    @brief  Class that stores state and functions for interacting with
@@ -37,7 +41,13 @@ public:
   bool begin(uint8_t address = AW9523_DEFAULT_ADDR, TwoWire *wire = &Wire);
   bool reset(void);
 
+  bool outputGPIO(uint16_t pins);
+  uint16_t inputGPIO(void);
+  bool configureGPIO(uint16_t pins);
+  bool openDrainPort0(bool od);
+
 protected:
+  uint16_t _pin_outputs = 0;
   Adafruit_I2CDevice *i2c_dev = NULL; ///< Pointer to I2C bus interface
 };
 
